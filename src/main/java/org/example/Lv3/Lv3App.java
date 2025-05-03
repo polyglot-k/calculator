b0dce4ad3858457efed0b6ccceebbb51182b9c25
KNU-K <knukang334@gmail.com>
2025-05-04 00:09:58 +0900

    feat: Lv3App 클래스 추가 및 사용자 입력에 따른 계산 기능 구현

---
+++ src/main/java/org/example/Lv3/Lv3App.java
@@ -0,0 +1,41 @@
+package org.example.Lv3;
+
+import org.example.Lv3.calculator.ArithmeticCalculator;
+import org.example.Lv3.calculator.OperatorType;
+
+import java.util.List;
+import java.util.Scanner;
+
+public class Lv3App {
+    public static void main(String[] args) {
+        try (Scanner scanner = new Scanner(System.in)) {
+            InputHandler inputHandler = new InputHandler(scanner);
+            ArithmeticCalculator calculator = new ArithmeticCalculator();
+
+            do {
+                Number operand1 = inputHandler.readOperand();
+                OperatorType operator = inputHandler.readOperator();
+                Number operand2 = inputHandler.readOperand();
+
+                try {
+                    double result = calculator.calculate(operand1, operand2, operator);
+                    System.out.println("결과: " + result);
+
+                    double threshold = inputHandler.readThreshold();
+                    List<Double> resultsGreaterThanThreshold = calculator.getResultsGreaterThan(threshold);
+                    System.out.println("임계치 (" + threshold + ") 보다 큰 결과 리스트: " + resultsGreaterThanThreshold);
+                    System.out.println("이전 연산 결과 : " + calculator.getResultHistories().toString());
+                    if (!inputHandler.shouldDeleteFirstResult()) continue;
+
+                    System.out.println("이전 연산 결과 : " + calculator.getResultHistories().toString());
+                    calculator.removeFirstResult();
+                    System.out.println("첫번째 결과 삭제 후 결과 : "+ calculator.getResultHistories().toString());
+
+                } catch (Exception e) {
+                    System.err.println("오류 발생: " + e.getMessage());
+                }
+            } while (inputHandler.isRetry());
+
+        }
+    }
+}
